05832b8fc323887157212d2db2d9d551dd46f68b
lojpark <lojpark@gmail.com>
2019-01-13 22:36:40 +0900

    Updates

---
+++ precache-manifest.6db919afb42f1f108d2a02a623fd8c6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "0069463c10dfe55d1764",
+    "url": "/movie_app/static/js/main.0069463c.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "0069463c10dfe55d1764",
+    "url": "/movie_app/static/css/main.cad32251.chunk.css"
+  },
+  {
+    "revision": "e325dc7d5d222ad4361e500cf73d13ab",
+    "url": "/movie_app/index.html"
+  }
+];
