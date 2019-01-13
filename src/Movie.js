d3c117943a6b0cc5804fe8fe41af41102460a7c4
lojpark <lojpark@gmail.com>
2019-01-13 22:05:39 +0900

    Render more information of movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './Movie.css';
 
-function Movie({title, poster}) {
+function Movie({title, poster, genres, synopsis}) {
     return (
-        <div>
-            <img src={poster} alt="Movie Poster"></img>
-            <h1>{title}</h1>
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <img src={poster} alt={title} />
+            </div>
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre) => 
+                        <span className="Movie__Genre">
+                            {genre}
+                        </span>)}
+                </div>
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+            </div>
         </div>
     )
 }
