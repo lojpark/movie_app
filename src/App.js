ad833010cda5d66a42a2ec3169ab2971b357e724
lojpark <lojpark@gmail.com>
2019-01-13 20:40:31 +0900

    Fetch the movie database

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import Movie from './Movie';
 
 class App extends Component {
+  componentDidMount() {
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div className="App">
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './Movie.css';
 
-class Movie extends Component{
-    render(){
-        return(
+class Movie extends Component {
+    render() {
+        return (
             <h1>Hello, this is a movie</h1>
         );
     }
